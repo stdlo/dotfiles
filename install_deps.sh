55a930569682e74623df867c12a23c2467bfbfbf
Logan Bickmore <loganbickmore91@gmail.com>
2018-04-24 14:16:47 -0700

    fixed wifi button/theme

--- install_deps.sh
+++ install_deps.sh
@@ -4,7 +4,7 @@
 sudo pacman -Syu --noconfirm
 
 # Pacman installs
-sudo pacman -S --noconfirm --needed git net-tools adobe-source-sans-pro-fonts acpi python2 python3 go i3-gaps i3blocks feh neovim yaourt htop wget ttf-hack tmux atom dunst cmake freetype2 fontconfig pkg-config make xclip xorg-xbacklight network-manager-applet rofi
+sudo pacman -S --noconfirm --needed git net-tools adobe-source-sans-pro-fonts acpi python2 python3 go i3-gaps i3blocks feh neovim yaourt htop wget ttf-hack tmux atom dunst cmake freetype2 fontconfig pkg-config make xclip xorg-xbacklight network-manager-applet rofi numix-gtk-theme
 
 # Yaourt installs
 yaourt -S --noconfirm --needed google-chrome ttf-iosevka cherry-font ttf-font-awesome-4
