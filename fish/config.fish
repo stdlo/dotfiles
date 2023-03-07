e00f2b0921499ac011b3ae4d932d2d6e17d71e56
Logan Bickmore <loganbickmore91@gmail.com>
2023-03-06 17:55:17 -0800

    updated load order

--- fish/config.fish
+++ fish/config.fish
@@ -3,6 +3,5 @@ if status is-interactive
     #bind \cC 'if test -z (commandline); echo -n; commandline | cat; commandline ""; commandline -f repaint; else; commandline -f cancel-commandline; end'
 end
 
-set PATH /usr/local/bin $PATH
 starship init fish | source
 thefuck --alias | source
