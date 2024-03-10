116fa719f885b7a59e2d829cae155ae88d535df9
stdlo <9201895+stdlo@users.noreply.github.com>
2024-03-10 15:46:23 -0700

    add nvim from github

--- home.nix
+++ home.nix
@@ -25,10 +25,50 @@
       wget
       yq
     ];
-    file.".config" = { source = ./dot_config; recursive = true; };
+    file.".config" = { source = ./home/config; recursive = true; };
     # file.".config/nvim" = { source = "https://github.com/stdlo/init.lua"; };
-    file.".gitconfig" = { source = ./dot_gitconfig; };
-    file.".tmux.conf" = { source = ./dot_tmux.conf; };
+    file.".gitconfig" = { source = ./home/gitconfig; };
+    file.".tmux.conf" = { source = ./home/tmux.conf; };
+    # Attempts at getting files from github
+    ### one ###
+    #     xdg.configFile."fish/themes/Catppuccin Latte.theme".source = let
+    #   catppuccin-fish = pkgs.fetchFromGitHub {
+    #     owner = "catppuccin";
+    #     repo = "fish";
+    #     rev = "0ce27b518e8ead555dec34dd8be3df5bd75cff8e";
+    #     hash = "sha256-Dc/zdxfzAUM5NX8PxzfljRbYvO9f9syuLO8yBr+R3qg=";
+    #   };
+    # in
+    #   "${catppuccin-fish}/themes/Catppuccin Latte.theme";
+    ### two ###
+    # file.".config/nvim".source = let
+    #   nvim = pkgs.fetchFromGitHub {
+    #     owner = "stdlo";
+    #     repo = "init.lua";
+    #   };
+    # in 
+    #   "${nvim}";
+    ### two again
+    file.".config/nvim".source = let
+      nvim = pkgs.fetchFromGitHub {
+        owner = "stdlo";
+        repo = "init.lua";
+        rev = "main";
+        hash = "sha256-/lTUe1blATNwTvGdOaxdcbfndNeWGCsf7PwJ29Rfqys=";
+      };
+    in 
+      "${nvim}";
+    ### three ###
+    # let
+    #   catppuccin-fish = pkgs.fetchFromGitHub {
+    #     owner = "catppuccin";
+    #     repo = "fish";
+    #     rev = "0ce27b518e8ead555dec34dd8be3df5bd75cff8e";
+    #     hash = "sha256-Dc/zdxfzAUM5NX8PxzfljRbYvO9f9syuLO8yBr+R3qg=";
+    #   };
+    # in {
+    #   xdg.configFile."fish/themes/Catppuccin Latte.theme".source = "${catppuccin-fish}/themes/Catppuccin Latte.theme";
+    # }
   };
   xdg.enable = true;
   programs.home-manager.enable = true;
@@ -37,10 +77,13 @@
     interactiveShellInit = ''
       set -gx EDITOR nvim
 
-      set -U fish_greeting
+      # set -U fish_greeting
 
-      fish_add_path $HOME/.local/bin
-      fish_add_path $HOME/.krew/bin
+      set -gx PATH $HOME/.local/bin $PATH
+      # set -gx PATH $HOME/.cargo/bin $PATH
+      set -gx PATH $PATH $HOME/.krew/bin
+      # fish_add_path $HOME/.local/bin
+      # fish_add_path $HOME/.krew/bin
 
       # disable interactive mode for kubectx/ns
       set -gx KUBECTX_IGNORE_FZF 1
