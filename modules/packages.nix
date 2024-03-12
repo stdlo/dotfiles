2e2248ab5ea94e762b979d65bb776d74bcc18b89
stdlo <9201895+stdlo@users.noreply.github.com>
2024-03-12 15:47:17 -0700

    cleanup packages, remove demo file

--- modules/packages.nix
+++ modules/packages.nix
@@ -1,21 +1,7 @@
-# { pkgs, builtins, ... }: let
-#   pkgs = import (builtins.fetchTarball {
-#       url = "https://github.com/NixOS/nixpkgs/archive/976fa3369d722e76f37c77493d99829540d43845.tar.gz";
-#       sha256 = "sha256:1r6c7ggdk0546wzf2hvd5a7jwzsf3gn1flr8vjd685rm74syxv6d";
-#   }) {};
-#
-#   kubectl = pkgs.kubectl;
-# in {
-# removed packages
-    # ansifilter
-    # moreutils #sponge
-    # colordiff
-    # bat
 { pkgs, ... }: {
   home.packages = with pkgs; [
-    # core packages
+    ## core packages
     atuin
-    # babelfish # fenv alternative
     curl
     eza
     fd
@@ -30,11 +16,18 @@
     unixtools.watch
     wget
     yq
-  ] ++ [
-    # work packages
+
+    ## work packages
     fnm
     kubectl
     kubectx
     kustomize
+
+    ## removed packages
+    # ansifilter
+    # bat
+    # colordiff
+    # moreutils #sponge
+    # navi
   ];
 }
